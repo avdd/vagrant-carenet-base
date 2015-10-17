9fb238bb4f6218a45f11a81b273b74dff549f7ab
avdd <i@avdd.co>
2015-10-17 16:01:44 +1100

    clean up properly

--- clean.sh
+++ clean.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -eu
 
-rm -rf /home/vagrant/Python*
+echo cleaning up ...
 apt-get clean
-rm /var/lib/dhcp3/*
+rm -rf /home/vagrant/Python*
+rm -f /var/lib/dhcp3/*
 rm -rf /dev/.udev/
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
 sync
 sync
 sync
-
+echo done.
