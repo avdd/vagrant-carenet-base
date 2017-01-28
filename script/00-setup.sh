e3b8d77bab64f4523f45220a5b3e6e68730e62e7
Adrian Dries <i@avdd.co>
2017-01-28 20:20:26 +1100

    install less stuff

--- script/00-setup.sh
+++ script/00-setup.sh
@@ -2,21 +2,25 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+apt-get update
+
+apt-get -qy install language-pack-en
+
+echo -n '' > /var/lib/locales/supported.d/en
+echo en_AU.UTF-8 UTF-8 >> /var/lib/locales/supported.d/local
+echo LANG=en_AU.UTF-8 > /etc/default/locale
+dpkg-reconfigure locales
+
+echo 'Australia/Sydney' > /etc/timezone
+dpkg-reconfigure -fnoninteractive tzdata
+
 apt-get -qy install \
-        language-pack-en \
         ncurses-term \
         libjpeg-dev \
-        libexpat1-dev \
-        apache2-mpm-prefork \
-        postgresql-8.3
+        libexpat1-dev
 
 apt-get -qy build-dep \
         python2.5 \
         python-psycopg2 \
         python-ldap
-
-echo en_AU.UTF-8 > /etc/default/locale
-dpkg-reconfigure locales
-echo 'Australia/Sydney' > /etc/timezone
-dpkg-reconfigure -fnoninteractive tzdata
 
